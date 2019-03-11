5d9871c4874ec3f59f8c2016266581ab78c2da92
Monkeylord <bluegod_001@hotmail.com>
2019-03-11 19:48:53 +0800

    first commit

---
+++ Index.js
@@ -0,0 +1,398 @@
+const crypto = require('crypto')
+const fs = require('fs')
+const explorer = require('bitcore-explorers')
+const Insight = explorer.Insight
+const insight = new explorer.Insight('https://api.bitindex.network')
+const bsv = require('bsv')
+const mime = require('mime')
+const ibe = require('bitcoin-ibe')
+const fetch = require('node-fetch')
+
+
+function Hammer(opts){
+  if (!(this instanceof Hammer)) {
+    return new Hammer(opts)
+  }
+  opts = opts || {}
+  var api = opts.api || 'https://api.bitindex.network'
+  this.insight = new Insight(api)
+  this.bitdb = opts.bitdb || 'https://genesis.bitdb.network/q/1FnauZ9aUH2Bex6JzdcV4eNX7oLSSEbxtN/'
+  this.bitdbKey = opts.bitdbKey || ['159bcdKY4spcahzfTZhBbFBXrTWpoh4rd3']
+  var privKey = opts.privKey || opts.privateKey || ""
+  if(privKey=="")throw new Error("Must Provide Private Key")
+  this.privKey = bsv.PrivateKey(privKey)
+  this.path = opts.path || './public'
+  this.utxos = null
+  this.sitemap = null
+  this.files = null
+  this.maptx = null
+  this.fileTXs = null
+  this.unBroadcastedTXs = []
+  return this
+}
+
+Hammer.prototype.throwout = function(){
+    console.log("Indexing files in " + this.path)
+    this.index().then(()=>{
+        console.log("Searching existing files on blockchain")
+        return this.reduceSitemap()
+    }).then(()=>{
+        console.log("Building upload transactions")
+        return this.build()
+    }).then(()=>{
+        console.log("Saving Sitemap to sitemap.json")
+        this.saveSitemap()
+        console.log("Broadcasting upload transactions")
+        return this.broadcast()
+    }).then(()=>{
+        console.log("Hammer Threw")
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+Hammer.prototype.index = function(){
+    return new Promise((resolve, reject)=>{
+        this.getFiles()
+        this.prepareSitemap()
+        resolve()
+    })
+}
+Hammer.prototype.build = function(){
+    return this.getUtxos().then(()=>{
+        if(!this.sitemap)throw new Error("No Sitemap found")
+        this.buildMapTX()
+        if(this.maptx.getFee()<this.maptx.toString().length)throw new Error("Insuffient Satoshis")
+        this.buildFileTXs()
+        return
+    })
+}
+
+Hammer.prototype.broadcast = function(){
+    return new Promise((resolve, reject)=>{
+        if(this.fileTXs.length<1)reject("No broadcast needed")
+        insight.broadcast(this.maptx.toString(),(err,res)=>{
+            if(err){
+                console.log("Insight API return Errors: ")
+                console.log(err)
+                reject("Insight API return Errors: " + err)
+            }else resolve()
+        })
+    }).then(()=>{
+        return Promise.all(this.fileTXs.map((fileTX)=>{
+                return new Promise((resolve,reject)=>{
+                    console.log("Broadcasting "+fileTX.hash)
+                    insight.broadcast(fileTX.toString(),(err,res)=>{
+                        if(err){
+                            console.log("Insight API return Errors")
+                            console.log("You may want to upload/replace fileTX:"+fileTX.hash)
+                            //fs.writeFileSync(fileTX.hash,fileTX.toString())
+                            this.unBroadcastedTXs.push({TXID:fileTX.hash,TX:fileTX.toString(),reason:err.message})
+                            resolve()
+                        }else{
+                            console.log(fileTX.hash + " Broadcasted")
+                            resolve()
+                        }
+                    })
+                })
+            })
+        )
+    }).then(()=>{
+        if(this.unBroadcastedTXs.length>0){
+            console.log("unBroadcasted TX: "+this.unBroadcastedTXs.length)
+            fs.writeFileSync("fileTXs.unBroadcasted.json",JSON.stringify(this.unBroadcastedTXs))
+        }else console.log("All TX Broadcasted")
+    })
+}
+
+Hammer.prototype.broadcast_continue = function(){
+    var TXs = JSON.parse(fs.readFileSync("fileTXs.unBroadcasted.json"))
+    return Promise.all(TXs.map((fileTX)=>{
+                return new Promise((resolve,reject)=>{
+                    console.log("Broadcasting "+fileTX.hash)
+                    insight.broadcast(fileTX.toString(),(err,res)=>{
+                        if(err){
+                            console.log("Insight API return Errors")
+                            console.log("You may want to upload/replace fileTX:"+fileTX.hash)
+                            //fs.writeFileSync(fileTX.hash,fileTX.toString())
+                            this.unBroadcastedTXs.push({TXID:fileTX.hash,TX:fileTX.toString(),reason:err.message})
+                            resolve()
+                        }else{
+                            console.log(fileTX.hash + " Broadcasted")
+                            resolve()
+                        }
+                    })
+                })
+            })
+        ).then(()=>{
+        if(this.unBroadcastedTXs.length>0){
+            console.log("unBroadcasted TX: "+this.unBroadcastedTXs.length)
+            fs.writeFileSync("fileTXs.unBroadcasted.json",JSON.stringify(this.unBroadcastedTXs))
+        }else console.log("All TX Broadcasted")
+    })
+}
+
+Hammer.prototype.getFiles = function(path){
+    // index all files
+    
+    if(!path){
+        if(this.path)
+        this.files = this.getFiles(this.path)
+        return
+    }
+    // console.log("Handling Directory " + path)
+    var files=[]
+    var items=fs.readdirSync(path)
+    items.forEach(item=>{
+        var itemPath = path+"/"+item
+        var stat = fs.statSync(itemPath)
+        if(stat.isDirectory()){
+            files = files.concat(this.getFiles(itemPath))
+        }else{
+            files.push(itemPath)
+        }
+    })
+    return files
+}
+
+Hammer.prototype.prepareSitemap = function(){
+    // Build a sitemap from files
+    
+    var path = this.path
+    if(!path.endsWith('/'))path = path + '/'
+    var map = {}
+    map.urls= {}
+    var sizecount = 0
+    this.files.map(file=>{
+        var stat = fs.statSync(file)
+        if (stat.size>64000) sizecount += Math.floor(stat.size / 64000) * 64000
+        sizecount += Math.max(stat.size % 64000, 546)
+        var buffer = fs.readFileSync(file)
+        var fsHash = crypto.createHash('md5')
+        fsHash.update(buffer);
+        var md5 = fsHash.digest('hex')
+        var url = file.startsWith(path)?file.slice(path.length):file
+        map.urls[url]={
+            path: file,
+            size: stat.size,
+            chunks: Math.ceil(stat.size/64000),
+            md5: md5,
+            txid: null
+        }
+    })
+    map.size = sizecount
+    this.sitemap = map
+    if(fs.existsSync('sitemap.proto.json')){
+        var sitemap_old = JSON.parse(fs.readFileSync('sitemap.proto.json'))
+        this.fillSitemap(sitemap_old)
+    }
+    return this.sitemap
+}
+
+Hammer.prototype.fillSitemap = function(sitemap_old){
+    // read another sitemap, if a file is already uploaded, mark it as uploaded in sitemap.
+    
+    var urls=Object.keys(this.sitemap.urls)
+    urls.forEach(url=>{
+        if(sitemap_old.urls[url]){
+            if(this.sitemap.urls[url].md5==sitemap_old.urls[url].md5){
+                if(this.sitemap.urls[url].txid==null){
+                    this.sitemap.urls[url].txid = sitemap_old.urls[url].txid
+                }
+            }
+        }
+    })
+    return this.sitemap
+}
+
+Hammer.prototype.trimSitemap = function(){
+    // get simplified sitemap
+    
+    var finalSitemap = {}
+    finalSitemap.urls = {}
+    var urls = Object.keys(this.sitemap.urls)
+    urls.forEach(url=>{
+        finalSitemap.urls[url]=this.sitemap.urls[url].txid
+    })
+    return finalSitemap
+}
+
+Hammer.prototype.buildMapTX = function(){
+    var urls=Object.keys(this.sitemap.urls)
+    var tx = bsv.Transaction()
+    // Inputs
+    this.utxos.forEach(utxo=>{
+        tx.from(utxo)
+    })
+    // Outputs
+    urls.forEach(url=>{
+        var info = this.sitemap.urls[url]
+        if(info.txid){
+            console.log("Skiping "+url)
+            return
+        }
+        info.utxos = []
+        for(var chunk=0;chunk<info.chunks;chunk++){
+            var fileKey = bsv.PrivateKey(this.privKey).childKey(info.md5 + chunk)
+            tx.to(fileKey.toAddress(),2 * (Math.min(info.size-chunk*64000,64000)+400))
+            var utxo = tx.outputs[tx.outputs.length-1].toJSON()
+            utxo.vout = tx.outputs.length-1
+            utxo.fileKey = fileKey.toString()
+            info.utxos.push(utxo)
+        }
+    })
+    // Change
+    // insight API issue, wrong fee amount. Have to calculate manually.
+    
+    tx.feePerKb(2048)
+    tx.change(this.privKey.toAddress())
+    /*
+    tx.to(this.privKey.toAddress(), tx.inputAmount - tx.outputAmount - 2 * tx.toString().length - 200)
+    */
+    if(tx.inputAmount-tx.outputAmount < tx.toString().length){
+        console.log("Insuffient Input Amount")
+    }
+    // Sign
+    tx.sign(this.privKey)
+    // Fill UTXO's txid
+    urls.forEach(url=>{
+        var info = this.sitemap.urls[url]
+        if(info.txid){
+            return
+        }
+        for(var chunk=0;chunk<info.chunks;chunk++){
+            var utxo = info.utxos[chunk]
+            utxo.txid = tx.hash
+        }
+    })
+    this.maptx = tx
+    return tx
+}
+
+Hammer.prototype.FileTX = function(fileInfo, chunk){
+    var utxo = fileInfo.utxos[chunk]
+    var tx = bsv.Transaction().from(utxo)
+    var fileKey = bsv.PrivateKey(fileInfo.utxos[chunk].fileKey)
+    var fileBuf = fs.readFileSync(fileInfo.path).slice(chunk*64000,(chunk+1)*64000)
+    var script = bsv.Script.buildDataOut("19HxigV4QyBv3tHpQVcUEQyq1pzZVdoAut")
+    script.add(fileBuf)
+    script.add(Buffer.from(mime.lookup(fileInfo.path)))
+    script.add(Buffer.from("binary"))
+    script.add(Buffer.from(fileInfo.md5))
+    var output = bsv.Transaction.Output({
+        satoshis: 0,
+        script: script.toHex()
+    })
+    tx.addOutput(output)
+    tx.sign(fileKey)
+    return tx
+}
+
+Hammer.prototype.saveSitemap = function(){
+    fs.writeFileSync("sitemap.proto.json",JSON.stringify(this.sitemap,null,4))
+    fs.writeFileSync("sitemap.json",JSON.stringify(this.trimSitemap(this.sitemap),null,4))
+}
+
+Hammer.prototype.saveFileTXs = function(){
+    var TXs = {}
+    if(fs.existsSync('fileTXs.json')){
+        TXs=JSON.parse(fs.readFileSync('fileTXs.json').toString())
+        fs.renameSync('fileTXs.json','fileTXs.old.json')
+    }
+    this.fileTXs.forEach(fileTX=>{
+        console.log("Saving " + fileTX.hash + " to fileTXs.json")
+        TXs[fileTX.hash]=fileTX.toString()
+    })
+    fs.writeFileSync('fileTXs.json',JSON.stringify(TXs, null, 4))
+}
+
+Hammer.prototype.getUtxos = function(){
+    return new Promise((resolve, reject)=>{
+        this.insight.getUnspentUtxos(this.privKey.toAddress(),(err,utxos)=>{
+            if(err){
+                reject("Insight API return Errors: " + err)
+            }else{
+                this.utxos = utxos
+                resolve(this)
+            }
+        })
+    })
+}
+
+Hammer.prototype.buildFileTXs = function(){
+    this.fileTXs = []
+    var urls=Object.keys(this.sitemap.urls)
+    urls.forEach(url=>{
+        var fileinfo = this.sitemap.urls[url]
+        if(fileinfo.txid){
+            // console.log("Skiping "+url)
+            return
+        }
+        for(var chunk=0;chunk<fileinfo.chunks;chunk++){
+            var tx = this.FileTX(fileinfo,chunk)
+            this.fileTXs.push(tx)
+            if(!fileinfo.txid)fileinfo.txid = []
+            fileinfo.txid[chunk] = tx.hash
+        }
+    })
+}
+
+Hammer.prototype.reduceSitemap = function(){
+    if(!(this.bitdb && this.bitdbKey))throw new Error("bitdb or bitdbKey not specified")
+    var urls=Object.keys(this.sitemap.urls)
+    return Promise.all(urls.map(murl=>{
+        return new Promise((resolve,reject)=>{
+            var fileinfo = this.sitemap.urls[murl]
+            if(fileinfo.txid){
+                resolve()
+                return
+            }
+            var fileBuf = fs.readFileSync(fileinfo.path)
+            var filechunks = []
+            for(var chunk=0;chunk<fileinfo.chunks;chunk++){
+                filechunks.push(fileBuf.slice(chunk*64000,(chunk+1)*64000))
+            }
+            
+            var chunks = new Array(fileinfo.chunks)
+            var query = {
+                "v": 3,
+                "q": {
+                    "find": { "out.s5": fileinfo.md5},
+                },
+                "r": {
+                    "f": "[ .[] | {content: .out[0].lb2, content_short: .out[0].b2, contenttype: .out[0].s3, txid: .tx.h} ]"
+                }
+            }
+            var b64 = Buffer.from(JSON.stringify(query)).toString('base64')
+            var url = this.bitdb + b64;
+            var header = {
+                headers: { key: this.bitdbKey }
+            }
+            resolve(fetch(url, header).then(function(r) {
+              return r.json()
+            }).then(function(r) {
+                r.c.forEach(entry=>{
+                    entry.content = Buffer.from(entry.content || entry.content_short, 'base64')
+                    for(var chunk=0;chunk<filechunks.length; chunk++){
+                        if(filechunks[chunk].equals(entry.content))chunks[chunk] = entry.txid
+                    }
+                })
+                return
+            }).then(()=>{
+                if(chunks.every(txid=>txid.length==64)){
+                    fileinfo.txid = chunks
+                }
+                return
+            }))
+        })
+    }))
+}
+
+
+function promiseTimeout (time) {
+  return new Promise(function(resolve,reject){
+    setTimeout(function(){resolve(time);},time);
+  });
+};
+
+module.exports = Hammer
--- README.md
+++ README.md
@@ -1,2 +1,155 @@
 # MetaSite-Hammer
 A metanet tool to upload static website to blockchain.
+
+### Usage
+
+This example upload `./public`, and `sitemap.json` is generated as sitemap for metasite-loader..
+
+~~~javascript
+const Hammer = require('metasite-hammer')
+var hammer = new Hammer({
+    privKey: 'A Private Key with enough satoshis'
+})
+
+hammer.throwout()
+// If there are more than 25 TXs to upload, you need to wait a confirmation to upload the rest.
+hammer.broadcast_continue()
+~~~
+
+#### Full Options Example
+
+~~~javascript
+var hammer = new Hammer({
+    privKey: 'L4nArjTrhucfLrAEZfHg9QLXDjWbJ1DfcPSDfT2Hwk98P7Kcfn7B',
+    path: './public',
+    api: 'https://api.bitindex.network',
+    bitdb: 'https://genesis.bitdb.network/q/1FnauZ9aUH2Bex6JzdcV4eNX7oLSSEbxtN/',
+    bitdbKey: ['159bcdKY4spcahzfTZhBbFBXrTWpoh4rd3'],
+})
+~~~
+
+### Install
+
+~~~shell
+npm install metasite-hammer
+~~~
+
+### What MetaSite-Hammer do?
+
+1. Indexing all files in `path`
+2. Finding out if any file already exists on chain and exclude them from uploading list.
+3. Building uploading TXs. A root TX is built first for allocating satoshis to outputs for each file chunk. Then file chunk TXs are built upon root TX.
+4. Now all file txid is known, save sitemap to `sitemap.json`.
+5. Broadcast root TX and file chunk TXs.
+
+### Sitemap Example
+
+~~~json
+{
+    "urls": {
+        "2019/03/01/The-First-Metanet-Blog/index.html": [
+            "34491b5a3bd6a66bf7e19c22f4d680dd7bb895e2681ed3b926d8bc81ceca4e35"
+        ],
+        "2019/03/04/The-Siteloader-is-Released/index.html": [
+            "50a4aabbab943a295c6a5e97d4845a10d18c8fbdf3f87f7e176a0c6676d88a88"
+        ],
+        "2019/03/11/Metanet应用开发/index.html": [
+            "c748ecf64bbe01ad0552a62db1a91519dec5cc7a21b8314c8a1bdede9af69e07"
+        ],
+        "archives/2019/03/index.html": [
+            "956d076dffd3940060c69ebda7d50e0faf6bf2a4d9c5a53bdfbd8a460ca27fbe"
+        ],
+        "archives/2019/index.html": [
+            "3027b2105f82891944960ec6ccb6a12b987ec4d8a45d6495cf4f9b92c9dec880"
+        ],
+        "archives/index.html": [
+            "0b782b4e1dce43e3301a6b87e47eab79c52316f3c225eacc08c40e48f0f9c567"
+        ],
+        "css/fonts/fontawesome-webfont.eot": [
+            "bb6428e7da0e2d30d279afbb6a831fa998b685612c7a09ebd0aee535692db4ac"
+        ],
+        "css/fonts/fontawesome-webfont.svg": [
+            "72e5ee805b99b1837745b043c27db0d8cd4f5ffeda4035623ab17b0e04190a68",
+            "880fc7a0588f3d484756c9b0e81dc06f3a3368afa6736a13f826c00c299bc390",
+            "a6c86073e80714ac2e3c1553b00afc3605a116417ad9eadcc1e784bad325cd17",
+            "775114a518e2c0907659631b7637770f1fc6b7283204c85ae068c7fcae8ef5bc"
+        ],
+        "css/fonts/fontawesome-webfont.ttf": [
+            "e2154b8b00cabf451b69107fe295d222f9b03783cb3d154ab0f053f8d73bb770",
+            "527c568628508b2afe6912db898054459d5e0937edf6a4126fefdeac64a2e7f1"
+        ],
+        "css/fonts/fontawesome-webfont.woff": [
+            "e57d22e9d7a3a3a44ad2015fa12de9a36ece6a512f60b97046bc3dcf822bf7ba"
+        ],
+        "css/fonts/FontAwesome.otf": [
+            "7854b991ee51fcbff3d902ed9f90aa6ef0d1d81f66669f51dbe7404620165b21"
+        ],
+        "css/images/banner.jpg": [
+            "9defd281159a9d0ba2b58f3f70b20f963ed07fa1f1c86ba50f2114f29e2cd554",
+            "3ea1db51622b46518312c6f261b065d8a5a2b39cf59dcda6aeb6924f9f698b87",
+            "5d73d06f3370534c6d4dcebcc31d97f46cbb438415ca8e1de9039158fe451e7c",
+            "692222086cb0be799b36158209498da663b2d8cc049bd9801e8b78462a36dd82"
+        ],
+        "css/style.css": [
+            "942312723da69e1ee57a40f21c3511c3d38a20b6d025b4429d26b1e73a81178c"
+        ],
+        "fancybox/blank.gif": [
+            "764b2d4ec3dfc1bbb28705dc5d52e4f9461c796a608af890332accb9cd6c3209"
+        ],
+        "fancybox/fancybox_loading.gif": [
+            "cf1322091b53fbc2213558fc976fcb90a4f373eaea3876b5f1d791f9cfb66c2d"
+        ],
+        "fancybox/fancybox_loading@2x.gif": [
+            "f647889e5cdf0fe79475e8a7b7f78d4fce50c4e7de3f4d8994644baed292e245"
+        ],
+        "fancybox/fancybox_overlay.png": [
+            "327297b1e6b5c857c9f0378b389abdcbe1598c127363d201da5278a2893ca7ed"
+        ],
+        "fancybox/fancybox_sprite.png": [
+            "bbb0e6416a4d6c33781ecd70c5f8532ccb7d17ab84c81a3aa9f2bb751a22096d"
+        ],
+        "fancybox/fancybox_sprite@2x.png": [
+            "6b5cfbc5923fa769408359ca065f2ab8ddcef4b2202923d8712852e8b8c00745"
+        ],
+        "fancybox/helpers/fancybox_buttons.png": [
+            "6a10e48705d92687cd776171f721f6a6129d15641bb73e047ef2e3b7d3b6a1ff"
+        ],
+        "fancybox/helpers/jquery.fancybox-buttons.css": [
+            "5f9d54907f06849ce290269ffbe90ec0274d2b35be97ff8a9f43a48907d652ae"
+        ],
+        "fancybox/helpers/jquery.fancybox-buttons.js": [
+            "03d03c9e6c9f6ba6d319a739a4b5f93ddbb2fcabd6427e7123f22e2d790fd0dc"
+        ],
+        "fancybox/helpers/jquery.fancybox-media.js": [
+            "d4c27f531eeee764fc9c86291bcf20787cf0fe11ad7e53ae40d6d42b80febf29"
+        ],
+        "fancybox/helpers/jquery.fancybox-thumbs.css": [
+            "0148dfb22f8fd926f3c8af5d83b7ec374325d6c81c5a406242fbbb0d0fc49647"
+        ],
+        "fancybox/helpers/jquery.fancybox-thumbs.js": [
+            "ef30c17f2bec318cbc2856909ca926a795d0042e0fb7544a9bdb5fed6a717182"
+        ],
+        "fancybox/jquery.fancybox.css": [
+            "dda3ebf4db9a266de5e75238b989a88fe4ce98ebb54933b95a9a00d681c1a595"
+        ],
+        "fancybox/jquery.fancybox.js": [
+            "94b9af9ca00b3bd05065a79822dbe6aa53c7e7e1c7adb4ba34a0ac50a2b788e4"
+        ],
+        "fancybox/jquery.fancybox.pack.js": [
+            "89d650c27a06910c825deb3a8e6757ceb34d2cd23325e4a7c35698158725bac2"
+        ],
+        "index.html": [
+            "9521958d3cb61c54bb090f389055d77ac7ca464d5fa3ebce3df3337bb4d9cdaa"
+        ],
+        "js/script.js": [
+            "ecace26ba169a8fc403cd46a34084016d3edfa4b78cc407fb3fa73d720c79a17"
+        ]
+    }
+}
+~~~
+
+### Future Development
+
+Extend Sitemap format
+
+Write a Tutorial
